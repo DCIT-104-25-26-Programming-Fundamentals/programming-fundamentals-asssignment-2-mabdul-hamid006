26789d9fdecdd5743498755d86dec02a53f48a9a
mabdul-hamid006 <mohammedabdulhamid646@gmail.com>
2026-08-02 17:49:47 +0000

    Completed fibonacci sequence assignment

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,58 @@
 #include <iostream>
 using namespace std;
 
+// PART A: Prints the first n Fibonacci terms
+void printFibonacci(int n) {
+    int a = 0, b = 1;
+    cout << "Fibonacci sequence: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << endl;
+}
+
+// PART B: Checks whether a given number is a Fibonacci number
+bool isFibonacci(int num) {
+    int a = 0, b = 1;
+
+    if (num == 0) return true;
+
+    while (a <= num) {
+        if (a == num) {
+            return true;
+        }
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    return false;
+}
+
+int main() {
+    int n;
+
+    cout << "How many terms? ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return 0;
+    }
+
+    printFibonacci(n);
+
+    int num;
+    cout << "Enter a number to check: ";
+    cin >> num;
+
+    if (isFibonacci(num)) {
+        cout << num << " is a Fibonacci number." << endl;
+    } else {
+        cout << num << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
